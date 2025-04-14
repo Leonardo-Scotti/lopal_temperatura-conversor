22b174c1a3c6f1852c8eb8e432ab7de6a2a5b86e
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-04-14 16:55:23 -0300

    Começando a tela gráfica

--- src/br/dev/leonardo/temperatura/TemperaturaApp.java
+++ src/br/dev/leonardo/temperatura/TemperaturaApp.java
@@ -1,10 +1,15 @@
 package br.dev.leonardo.temperatura;
 
+import br.dev.leonardo.temperatura.gui.TelaConversor;
+
 public class TemperaturaApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		TelaConversor tela = new TelaConversor();
+		tela.criarTelaConversor();
+		
 	}
 
 }
--- src/br/dev/leonardo/temperatura/gui/TelaConversor.java
+++ src/br/dev/leonardo/temperatura/gui/TelaConversor.java
@@ -1,14 +1,17 @@
 package br.dev.leonardo.temperatura.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class TelaConversor {
 	
 	//Declarando Atributos
-	private JTextField textCelcius;
-	private JLabel labelCelcius;
+	private JTextField textCelsius;
+	private JLabel labelCelsius;
 	private JButton buttonKelvin;
 	private JButton buttonFahreinheit;
 	private JLabel labelResultado;
@@ -17,6 +20,27 @@ public class TelaConversor {
 	//Método para criar a tela
 	public void criarTelaConversor() {
 		
+		//Criando objeto JFrame
+		JFrame tela = new JFrame();
+		
+		//Definindo o tamanho da tela
+		//através de um objeto Dimension
+		Dimension tamanho = new Dimension();
+		tamanho.setSize(500, 400);
+		
+		//Setando tamanho da tela através do Dimension
+		tela.setSize(tamanho);
+		
+		
+		//Celsius
+		
+		
+		
+		
+		
+		
+		
+		tela.setVisible(true);
 	}
 	
 }
