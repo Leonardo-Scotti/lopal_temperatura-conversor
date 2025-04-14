33eb94d45bac76c9c2742923dec71997f4f49355
LEONARDO SCOTTI TOBIAS <25132821@CFP127ALUNO.LOCAL>
2025-04-14 16:35:08 -0300

    Finalizada a Estutura de Pacotes

---
+++ src/br/dev/leonardo/temperatura/gui/TelaConversor.java
@@ -0,0 +1,22 @@
+package br.dev.leonardo.temperatura.gui;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TelaConversor {
+	
+	//Declarando Atributos
+	private JTextField textCelcius;
+	private JLabel labelCelcius;
+	private JButton buttonKelvin;
+	private JButton buttonFahreinheit;
+	private JLabel labelResultado;
+	private JLabel lopalMensagemErro;
+	
+	//Método para criar a tela
+	public void criarTelaConversor() {
+		
+	}
+	
+}
---
+++ src/br/dev/leonardo/temperatura/model/Temperatura.java
@@ -0,0 +1,27 @@
+package br.dev.leonardo.temperatura.model;
+
+public class Temperatura {
+	
+	//Declarando Atributos
+	private double celsius;
+	
+	//Métodos
+	
+	//Métodos Setters e Getters
+	public double getCelsius( ) {
+		return celsius;
+	}
+	
+	public void setCelsius(double celsius) {
+		this.celsius = celsius;
+	}
+	
+	//Métodos para conversão
+	public double converterParaKelvin( ) {
+		
+	}
+	
+	public double converterParaFahreinheit( ) {
+		
+	}
+}
